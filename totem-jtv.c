cd72a3663fe4396ec842e0c709ff168f2e7f626c
Mohammed Sameer <msameer@foolab.org>
2012-03-11 16:50:37 +0200

    Moved to autotools

--- totem-jtv.c
+++ totem-jtv.c
@@ -1,12 +1,33 @@
+/*
+ * Copyright (C) 2012 Mohammed Sameer <msameer@foolab.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <totem-plugin.h>
 #include <totem.h>
 #include <libsoup/soup.h>
 #include <gtk/gtk.h>
 #include <libxml/parser.h>
 #include <libxml/xpath.h>
-
-// TODO:
-#define GETTEXT_PACKAGE "jtv"
 #include <glib/gi18n-lib.h>
 
 #define TOTEM_TYPE_JTV_PLUGIN               (totem_jtv_plugin_get_type ())
